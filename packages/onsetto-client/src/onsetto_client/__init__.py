967a08264204b34a8395c7f99e0cae27f90faf5c
Will Mooney <will.mooney.3@gmail.com>
2026-06-20 17:26:05 -0400

    Initial setup of each package using uv

---
+++ packages/onsetto-client/src/onsetto_client/__init__.py
@@ -0,0 +1,3 @@
+"""onsetto_client: Typed Python SDK for the Onsetto account API."""
+
+__all__: list[str] = []
---
+++ packages/onsetto-scraper/src/onsetto_scraper/__init__.py
@@ -0,0 +1,3 @@
+"""onsetto_scraper: Playwright-based browser automation for the Onsetto account flow."""
+
+__all__: list[str] = []
